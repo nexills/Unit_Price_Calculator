271a34a6616f14027c4650660d3fc2e5f83746a4
NevilleSFU <ntl11@sfu.ca>
2023-09-01 17:47:23 -0700

    Added new functions

--- function.py
+++ function.py
@@ -5,29 +5,44 @@ class item:
 
     # class atributes
 
-    def __init__(self, gui, std_font):   
+    def __init__(self, gui, std_font, number):
+        # setting up labels and entries   
         self.price = tk.Entry(gui, bg='grey', fg='white', font=std_font, 
-                              justify="right")
+                              justify="right", width = 7)
         self.size = tk.Entry(gui, bg='grey', fg='white', font=std_font, 
-                             justify="right")
+                             justify="right", width = 7)
         self.discount = tk.Entry(gui, bg='grey', fg='white', font=std_font, 
-                                 justify="right")
-        self.percentage = tk.Label(gui, text='%', font=std_font)
+                                 justify="right", width = 7)
+        self.num_items = tk.Entry(gui, bg='grey', fg='white', font=std_font, 
+                                 justify="right", width = 7)
+        self.title = tk.Label(gui, text='Item ' + str(number), font=std_font)
+        self.percentage = tk.Label(gui, text='%', font=std_font, width = 1)
         self.pricelabel = tk.Label(gui, text='Price:', font=std_font)
         self.sizelabel = tk.Label(gui, text='Size(g/lb/etc.):', font=std_font)
+        self.numlabel = tk.Label(gui, text='Number of items:', font=std_font)
         self.discountlabel = tk.Label(gui, text='Discount:', font=std_font)
         self.row = -1
         self.col = -1
+        # special labels
         self.resultlabel = tk.Label(gui, text='', font=std_font)
+        # default entry values
+        self.price.insert(tk.END, '0')
+        self.size.insert(tk.END, '1')
+        self.discount.insert(tk.END, '0')
+        self.num_items.insert(tk.END, '1')
 
     def create(self, row, col):
-        self.price.grid(row=row, column=col + 1)
-        self.size.grid(row=row + 1, column=col + 1)
-        self.discount.grid(row=row + 2, column=col + 1)
-        self.pricelabel.grid(row=row, column=col)
-        self.sizelabel.grid(row=row + 1, column=col)
-        self.discountlabel.grid(row=row + 2, column=col)
-        self.percentage.grid(row=row+2, column=col+2)
+        # put everything on screen
+        self.title.grid(row=row, column=col)
+        self.price.grid(row=row+1, column=col + 1)
+        self.size.grid(row=row + 2, column=col + 1)
+        self.discount.grid(row=row + 4, column=col + 1)
+        self.pricelabel.grid(row=row+1, column=col)
+        self.sizelabel.grid(row=row + 2, column=col)
+        self.discountlabel.grid(row=row + 4, column=col)
+        self.percentage.grid(row=row+4, column=col+2)
+        self.num_items.grid(row=row+3, column=col+1)
+        self.numlabel.grid(row=row+3, column=col)
         self.row = row
         self.col = col
 
@@ -38,13 +53,15 @@ class item:
             price = float(self.price.get())
             size = float(self.size.get())
             discount = float(self.discount.get())
+            num_items = float(self.num_items.get())
         except ValueError:
-            # happens when some fields are empty
-            self.resultlabel['text'] = 'Field(s) are empty!'
+            # happens when some fields are empty or have non-numbers
+            self.resultlabel['text'] = 'Illegal values!'
+            self.unit_price = -1
         else:
-            unit_price = price/size * (1-discount*0.01)
-            self.resultlabel['text'] = str(round(unit_price, 4))
+            self.unit_price = price/size * (1-discount*0.01) / num_items
+            self.resultlabel['text'] = '$' + str(round(self.unit_price, 4)) + '/unit'
         finally:
-            self.resultlabel.grid(row=self.row+3, column=self.col+1)    
+            self.resultlabel.grid(row=self.row+5, column=self.col+1)    
 
 
--- main.py
+++ main.py
@@ -9,43 +9,39 @@ def calculate_all(item_list):
     min_index = -1
     for i in range(len(item_list)):
         item_list[i].calculate()
-        try: 
-            float(item_list[i].resultlabel['text'])
-        except:
-            # happens if some fields in the item is empty
+        if item_list[i].unit_price == -1:
             continue
         else:
             # set new minimum if less than or first item
             if (minimum == -1):
-                minimum = float(item_list[i].resultlabel['text'])
+                minimum = item_list[i].unit_price
                 min_index = 0
-            elif (float(item_list[i].resultlabel['text']) < minimum):
-                minimum = float(item_list[i].resultlabel['text'])
+            elif (item_list[i].unit_price < minimum):
+                minimum = item_list[i].unit_price
                 min_index = i
     if (min_index == -1):
         return
     result = tk.Label(gui, text='Cheapest item: item ' + str(min_index+1)
                       , font=std_font)
-    result.grid(row=11, column=1)
+    result.grid(row=20, column=1)
 
 
 
 if __name__ == '__main__':
     gui = tk.Tk()
     # set up the window
-    gui.geometry("600x800")
     gui.title("Calculator")
     # set up the gui elements
     std_font = ('calibre',14,'normal') # define this as the standard font
 
-    item_1 = fn.item(gui, std_font)
+    item_1 = fn.item(gui, std_font, 1)
     item_1.create(0,0)
-    item_2 = fn.item(gui, std_font)
-    item_2.create(5,0)
+    item_2 = fn.item(gui, std_font, 2)
+    item_2.create(6,0)
     item_list = [item_1, item_2]
     execute = tk.Button(gui, text='Calculate', font=std_font, 
                         command=lambda: calculate_all(item_list))
-    execute.grid(row=10, column=1)
+    execute.grid(row=21, column=1)
 
 
     gui.mainloop()
